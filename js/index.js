f982c08d5c2e050506f63d9e9b8f958b8464fce4
Jeremy Eaton <jeremyeaton89@gmail.com>
2015-08-12 22:27:38 -0700

    improve proficiency visualization

--- js/index.js
+++ js/index.js
@@ -12,6 +12,52 @@ $(function() {
     paginationSpeed: 500,
   });
 
+  // Proficiency
+  var page1widths = [86,85,90,90,70,60];
+  var page2widths = [88,85,80,40,60,60];
+  var page1       = true;
+
+  function showPage1() {
+    $('#experience_labels li.page1').fadeOut(300, function() {
+      $('#experience_labels li.page2').fadeIn(800);
+    });
+    $('#experience_chart li').each(function(i, el) { 
+      $(el).delay(500).animate({'width': (page2widths[i] + '%')}, 500);
+    });
+  }
+  function showPage2() {
+    $('#experience_labels li.page2').fadeOut(300, function() {
+      $('#experience_labels li.page1').fadeIn(800);
+    });
+    $('#experience_chart li').each(function(i, el) {
+      $(el).delay(500).animate({'width': (page1widths[i] + '%')}, 500);
+    });
+  }
+  function handlePaging() {
+    if (page1) {
+      page1 = false;
+      showPage2();
+    } else {
+      page1 = true;
+      showPage1();
+    }
+  }
+
+  var interval = setInterval(function() {
+    handlePaging();
+  }, 10000);
+
+  // toggle. stop paging for 20 seconds, then resume. 
+  $('#experience_chart').click(function() {
+    clearInterval(interval);
+    handlePaging();
+    setTimeout(function() {
+      interval = setInterval(function() {
+        handlePaging();
+      }, 10000);
+    }, 20000)
+  })
+
   // Blog Pagination
   $('#blog_pagination a').click(function() {
     if ($(this).is('active')) return;
